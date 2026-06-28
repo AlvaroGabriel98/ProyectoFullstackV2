06d56ddb39c558204f48217db8c6f8c49219080f
IgnacioMontecinosduoc <ig.montecinosd@duocuc.cl>
2026-06-28 16:35:02 -0400

    agregue pedidos

---
+++ ms-pedidos/ms-pedidos/src/main/repository/PedidoDetalleRepository.java
@@ -0,0 +1,9 @@
+package com.ms_pedidos.repository;
+
+import com.gaming.mspedidos.entity.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+}
+
+
