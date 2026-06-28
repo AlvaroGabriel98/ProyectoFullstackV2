4a3a137a551678c3af5e2827f565b538233e2747
Ignacio Montecinos <ig.montecinosd@duocuc.cl>
2026-06-28 00:34:31 -0400

    arregle errores en user-services para dejarlo levantado y funcionando

---
+++ user-service/src/main/java/com/user_service/exception/BussinesException.java
@@ -0,0 +1,9 @@
+package com.user_service.exception;
+
+public class BussinesException extends RuntimeException {
+
+    public BussinesException(String message) {
+        super(message);
+    }
+
+}
---
+++ user-service/src/main/java/com/user_service/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.user_service.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
